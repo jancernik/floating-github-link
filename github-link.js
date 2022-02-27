6a8c52ca460da65cabcdc464b960de9c3a3da8e6
Jan Cernik <jancernik12@gmail.com>
2022-02-27 17:53:34 -0300

    -

---
+++ github-link.js
@@ -0,0 +1,36 @@
+const fontAwesomeJsSource = "https://kit.fontawesome.com/75e6554cbc.js";
+const cssSource = "github-link-style.css";
+
+const head = document.querySelector("head");
+
+if (!checkIfElementExists("script", "src", fontAwesomeJsSource)) {
+  const fontAwesome = document.createElement("script");
+  fontAwesome.setAttribute("src", fontAwesomeJsSource);
+  fontAwesome.setAttribute("crossorigin", "anonymous");
+  head.append(fontAwesome);
+}
+
+if (!checkIfElementExists("link", "href", cssSource)) {
+  const githubLinkStyle = document.createElement("link");
+  githubLinkStyle.setAttribute("rel", "stylesheet");
+  githubLinkStyle.setAttribute("href", cssSource);
+  head.append(githubLinkStyle);
+}
+
+window.addEventListener("load", function () {
+  if (checkIfElementExists("a", "class", "github-link")) {
+    const githubLink = document.querySelector(".github-link");
+    if (!githubLink.hasAttribute("target")) {
+      githubLink.setAttribute("target", "_blank");
+    }
+  }
+});
+
+function checkIfElementExists(target, attribute, attributeContent) {
+  const elements = document.querySelectorAll(target);
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].getAttribute(attribute).includes(attributeContent))
+      return true;
+  }
+  return false;
+}
